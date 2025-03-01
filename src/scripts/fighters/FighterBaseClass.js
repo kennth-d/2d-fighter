225174b6f430b82a3de122e9b8d837775ea85b06
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-01 10:00:36 -0500

    -direction now uses OpponentDirection enum - constructor inizializes origin.x/y to 0.

--- src/scripts/fighters/FighterBaseClass.js
+++ src/scripts/fighters/FighterBaseClass.js
@@ -1,14 +1,14 @@
-
+import { OpponentDirection } from "../utils/global.js";
 //class for maintaining the characters
 export class FighterBaseClass {
     constructor(x, y, playerId) {
         this.playerId= playerId;
-        this.direction = (playerId) ? -1 : 1; //value 1 = face right, -1 face left.
+        this.direction = (!playerId) ? OpponentDirection.RIGHT : OpponentDirection.LEFT;
         this.debug = false;
         this.health = 100;
         this.energy = 100;
         this.pos = {x: x, y: y};
-        this.origin = {x: x, y: y};
+        this.origin = {x: 0, y: 0};
         this.velocity = {x: 0, y: 0};
         
         //this.currentFrame = 0;    
