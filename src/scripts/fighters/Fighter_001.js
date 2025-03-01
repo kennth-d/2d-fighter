9968f549875b77f7ea687ecca8ea5bf1048be92f
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-01 09:57:11 -0500

    -update() now updates fighter.origin points. -added oponent field so that stateManager can update fighters direction based on the opponent.

--- src/scripts/fighters/Fighter_001.js
+++ src/scripts/fighters/Fighter_001.js
@@ -1,17 +1,23 @@
 import { FighterBaseClass } from "./FighterBaseClass.js";
-
 //Fighter_001 class
 export class Fighter_001 extends FighterBaseClass {
     constructor(x, y, playerId, inputComponent, stateManager, spriteManager) {
         super(x, y, playerId);
+        this.opponent = undefined;
 
         this.stateManager = stateManager;
         this.spriteManager = spriteManager;
         this.input = inputComponent;
+        this.input.id = playerId;
     }//end ctor
 
     update() {
-
+        
+        //update origin points
+        let offset = this.spriteManager.currentSprite.originOffset;
+        this.origin.x =  this.pos.x + ((this.spriteManager.currentSprite.img.width - offset) / 2);
+        this.origin.y = this.pos.y + (this.spriteManager.currentSprite.img.height - 8);
+        
         //update the fighter state
         this.stateManager.activeState.update(this.stateManager, this.input);
 
