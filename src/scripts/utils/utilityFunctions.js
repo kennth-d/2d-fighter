9cf7dabaa34c8c0c28352c589b2ca3acd8f2fdc0
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-01 10:06:51 -0500

    -added getDirection() helper function.

--- src/scripts/utils/utilityFunctions.js
+++ src/scripts/utils/utilityFunctions.js
@@ -1,13 +1,22 @@
-import { LEFT_BOUNDARY, RIGHT_BOUNDARY } from "./global.js";
+import { LEFT_BOUNDARY, RIGHT_BOUNDARY, OpponentDirection } from "./global.js";
 
 //Utility Functions
 export function EnsureOnScreen(fighter) {
-    let cushion = 10;
+    let cushion = 10; // ensures sprite stays within bounds.
     let xPos = fighter.origin.x;
     
     if (xPos <= LEFT_BOUNDARY + cushion) {
         fighter.pos.x = (fighter.pos.x - xPos) + cushion;
     } else if (xPos >= RIGHT_BOUNDARY - cushion) {
        fighter.pos.x = RIGHT_BOUNDARY + (fighter.pos.x - xPos) - cushion;
-    }
+    }//end if-else if
 }//end isAtLeftBoundary
+
+export function getDirection(player, opponent) {
+    if (player.origin.x >= opponent.origin.x) {
+        return OpponentDirection.LEFT;
+    } else {
+        return OpponentDirection.RIGHT;
+    }//end if-else
+}// end getDirection
+
