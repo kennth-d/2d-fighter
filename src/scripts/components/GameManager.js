f206a5c6f5eb29612c408ba9a69feb89df2757a1
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-07 14:01:42 -0500

    GameManager class created to manage the game state.

---
+++ src/scripts/components/GameManager.js
@@ -0,0 +1,75 @@
+import {Fighter_001} from "../fighters/Fighter_001.js";
+import {Fighter_002} from "../fighters/Fighter_002.js";
+import { KeyboardInputComponent } from "../components/KeyboardInputComponent.js";
+import { TIME, PLAYER_ONE_START, PLAYER_TWO_START, CANVAS_WIDTH, CANVAS_HEIGHT } from "../utils/global.js";
+import { StatusBar } from "../overlays/StatusBar.js";
+
+export class GameManager {
+    constructor(debug=false) {
+        this.ctx = this.getContext();
+
+        this.fighters = [
+            new Fighter_001(PLAYER_ONE_START.x, PLAYER_ONE_START.y, 0, new KeyboardInputComponent()),
+            new Fighter_002(PLAYER_TWO_START.x, PLAYER_TWO_START.y, 1, new KeyboardInputComponent()),
+        ];
+
+        this.fighters[0].opponent = this.fighters[1];
+        this.fighters[1].opponent = this.fighters[0];
+
+        this.entities = [
+            ...this.fighters,
+            new StatusBar(this.fighters[0], this.fighters[1]),
+        ];
+
+        if (debug){
+            this.setupDebug();
+        }//end if
+        
+    }//end ctor
+    getContext() {
+        const canvas = document.querySelector('canvas');
+        const ctx = canvas.getContext('2d');
+        return ctx;
+    }//end getContext
+    update(){
+        for (const entity of this.entities) {
+            entity.update();
+        }//end for
+    }//end update
+    draw() {
+
+        //draw background
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    
+        //draw entities
+        for(const entity of this.entities) {
+            entity.draw(this.ctx);
+        }//end for
+    }//end draw
+    frame(timestamp) {
+        window.requestAnimationFrame(this.frame.bind(this));
+
+        //update time
+        TIME.delta = (timestamp - TIME.previous) / 1000;
+        TIME.previous = timestamp;
+
+        this.update();
+        this.draw();
+    }//end frame
+    setupDebug() {
+        window.addEventListener("keypress", (e) => {
+            if (e.code === "Space") {
+                console.log(this.fighters[0]);
+                console.log(this.fighters[1]);
+            }//end if
+            if (e.code === "KeyV") {
+                this.fighters[0].debug = Boolean(Math.abs(this.fighters[0].debug - 1));
+                this.fighters[1].debug = Boolean(Math.abs(this.fighters[1].debug - 1));
+            }//end if
+        });
+    }//end setupDebug
+    start() {
+        window.requestAnimationFrame(this.frame.bind(this));
+    }//end start
+}//end GameManager
