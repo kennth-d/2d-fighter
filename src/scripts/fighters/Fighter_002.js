f19ae9c87ae30aa557fb5931f0a14002dd64ee97
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-07 14:01:20 -0500

    -F002 created to be separate from F001.

---
+++ src/scripts/fighters/Fighter_002.js
@@ -0,0 +1,59 @@
+import { FighterBaseClass } from "./FighterBaseClass.js";
+import { correctDirection } from "../utils/utils.js";
+import { resolveCollision } from "../utils/collision.js";
+import { FighterStateManager } from "../components/FighterStateManager.js";
+import { FighterSpriteManager } from "../components/SpriteManager.js";
+import { F002_SpriteData } from "../../assets/data/F002_SpriteData.js";
+//Fighter_001 class
+export class Fighter_002 extends FighterBaseClass {
+    constructor(x, y, playerId, inputComponent) {
+        super(x, y, playerId);
+
+        this.opponent = undefined;
+        this.stateManager = new FighterStateManager(this);
+        this.spriteManager = new FighterSpriteManager(F002_SpriteData);
+        this.input = inputComponent;
+        this.input.id = playerId;
+    }//end ctor
+
+    update() {
+        
+        //ensure player is facing the enemy
+        correctDirection(this, this.opponent);
+
+        //update origin points
+        this.updateOrigin();
+        
+        //update pushBox
+        this.updatePushbox();
+
+        //check and resolve collisions
+        resolveCollision(this);
+
+        //update the fighter state
+        this.stateManager.activeState.update(this.stateManager, this.input);
+
+        //update the sprite
+        this.spriteManager.update(this);
+    }//end update
+
+    draw(ctx) {
+        this.spriteManager.drawSprite(ctx, this);
+    }//end draw
+
+    updateOrigin() {
+        let offsetX = this.spriteManager.currentSprite.originOffset.x;
+        let offsetY = this.spriteManager.currentSprite.originOffset.y;
+        this.origin.x =  this.pos.x + ((this.spriteManager.currentSprite.img.width - offsetX) / 2);
+        this.origin.y = this.pos.y + (this.spriteManager.currentSprite.img.height - offsetY);
+    }
+    updatePushbox() {
+        let spriteWidth = this.spriteManager.currentSprite.pushBox.width;
+        let spriteHeight = this.spriteManager.currentSprite.pushBox.height;
+        this.pushBox.x = this.origin.x - (spriteWidth/2);
+        this.pushBox.y = this.origin.y - spriteHeight;
+        this.pushBox.width = spriteWidth;
+        this.pushBox.height = spriteHeight;
+    }
+}//end Fighter_001
+
