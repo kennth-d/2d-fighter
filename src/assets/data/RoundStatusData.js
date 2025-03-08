5557ec1d6bc340edd465fe950978b307d6ce8b13
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-08 15:05:10 -0500

    added round text image and data

---
+++ src/assets/data/RoundStatusData.js
@@ -0,0 +1,8 @@
+export const ROUND_STATUS = {
+    img   : document.querySelector('img[alt="roundText"]'),
+    KO    : { x: 2,   y: 179, width: 129, height: 83, orign: { x: 66, y: 262 } },
+    FIGHT : { x: 2,   y: 93,  width: 199, height: 83, orign: { x: 101, y: 176} },
+    3     : { x: 2,   y: 4,   width: 56,  height: 83, orign: { x: 27, y: 83  } },
+    2     : { x: 64,  y: 4,   width: 56,  height: 83, orign: { x: 91, y: 83  } },
+    1     : { x: 125, y: 5,   width: 36,  height: 83, orign: { x: 143, y: 83 } },
+};
