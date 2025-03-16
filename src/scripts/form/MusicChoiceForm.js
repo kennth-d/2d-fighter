72685cfe7e39238c329b848bd380facd9cbfbc77
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-16 16:22:04 -0400

    Container for a group of choices.

---
+++ src/scripts/form/MusicChoiceForm.js
@@ -0,0 +1,52 @@
+import { SETTINGS_CHOICES, CHOICE_PADDING, SETTINGS_CHOICE_WIDTH } from "../../assets/data/choiceData.js";
+import { DEFAULT_SETTINGS } from "../utils/global.js";
+import { drawLabel } from "./drawLabel.js";
+import { SettingsChoice } from "./SettingsChoice.js";
+
+const NUMBER = {
+    "OFF": 0,
+    "ON": 1,
+}
+export class MusicChoiceForm {
+    constructor(x, y) {
+        this.id = "music";
+        this.pos = {x, y};
+        this.selection = DEFAULT_SETTINGS.music;
+        this.choices = [
+            new SettingsChoice(SETTINGS_CHOICES.OFF, "OFF"),
+            new SettingsChoice(SETTINGS_CHOICES.ON, "ON"),
+        ];
+        //set rect properties
+        for (let i=0; i < this.choices.length; i++) {
+            x = this.pos.x + (i * SETTINGS_CHOICE_WIDTH) +  (i * CHOICE_PADDING);
+            y = this.pos.y;
+            this.choices[i].setRectProperty("x", x);
+            this.choices[i].setRectProperty("y", y); 
+        }//end for
+
+        this.choices[NUMBER[this.selection]].select();
+    }//ctor
+    getChoices() {
+        return this.choices;
+    }
+    draw(ctx) {
+        
+        drawLabel(ctx, "MUSIC", this.pos.x - 108, this.pos.y + 7);
+
+        for (const choice of this.choices) {
+            choice.draw(ctx);
+        }//end for
+    }//end draw
+    getSelection() {
+        return this.selection;
+    }//end getSelection
+    setSelection(newSelection) {
+
+        //delect choice
+        this.choices[NUMBER[this.selection]].deselect();
+
+        this.selection = newSelection;
+        //set seleciton
+        this.choices[NUMBER[this.selection]].select();
+    }//setSelection
+}//end RoundChoiceForm
---
+++ src/scripts/form/RoundChoiceForm.js
@@ -0,0 +1,51 @@
+import { SETTINGS_CHOICES, CHOICE_PADDING, SETTINGS_CHOICE_WIDTH } from "../../assets/data/choiceData.js";
+import { DEFAULT_SETTINGS } from "../utils/global.js";
+import { drawLabel } from "./drawLabel.js";
+import { SettingsChoice } from "./SettingsChoice.js";
+
+export class RoundChoiceForm {
+    constructor(x, y) {
+        this.id = "rounds";
+        this.pos = {x, y};
+        this.selection = DEFAULT_SETTINGS.rounds;
+        this.choices = [
+            new SettingsChoice(SETTINGS_CHOICES[1], 1),
+            new SettingsChoice(SETTINGS_CHOICES[2], 2),
+            new SettingsChoice(SETTINGS_CHOICES[3], 3),
+            new SettingsChoice(SETTINGS_CHOICES[4], 4),
+            new SettingsChoice(SETTINGS_CHOICES[5], 5),
+        ];
+        //set rect properties
+        for (let i=0; i < this.choices.length; i++) {
+            x = this.pos.x + (i * SETTINGS_CHOICE_WIDTH) +  (i * CHOICE_PADDING);
+            y = this.pos.y;
+            this.choices[i].setRectProperty("x", x);
+            this.choices[i].setRectProperty("y", y); 
+        }//end for
+
+        this.choices[this.selection - 1].select();
+    }//ctor
+    getChoices() {
+        return this.choices;
+    }
+    draw(ctx) {
+        
+        drawLabel(ctx, "NUMBER_OF_ROUNDS", this.pos.x - 225, this.pos.y + 7);
+
+        for (const choice of this.choices) {
+            choice.draw(ctx);
+        }//end for
+    }//end draw
+    getSelection() {
+        return this.selection;
+    }//end getSelection
+    setSelection(newSelection) {
+        //delect choice
+        this.choices[this.selection - 1].deselect();
+
+        //set seleciton
+        this.selection = newSelection;
+        this.choices[this.selection - 1].select();
+    }//setSelection
+
+}//end RoundChoiceForm
