e3b8f38e6851c7e087763a9cd99a2b9fb0c51a67
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-16 16:22:28 -0400

    Ranged input class to draw on canvas.

---
+++ src/scripts/form/RangeInput.js
@@ -0,0 +1,19 @@
+// https://stackoverflow.com/questions/39628400/range-input-on-canvas
+
+export class RangeInput {
+    constructor(min, max, dimensions) {
+        this.dimensions = dimensions;
+        this.min = min;
+        this.max = max;
+        this.value;
+    }
+    getValue() {
+        return this.value;
+    }
+    update() {
+        
+    }
+    draw() {
+
+    }
+}
