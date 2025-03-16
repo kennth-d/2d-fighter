ba3d90cdaea3a103c9ecdd9092721e0157dc110e
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-16 16:19:06 -0400

    stores attack type and damage. and max health/energy values

---
+++ src/scripts/utils/battle.js
@@ -0,0 +1,26 @@
+export const MAX_HEALTH = 100;
+export const MAX_ENERGY = 100;
+
+ 
+export const ATTACK_TYPE = {
+    LIGHT: {
+        TYPE: "LIGHT",
+        DMG: 3,
+    },
+    HEAVY: {
+        TYPE: "HEAVY",
+        DMG: 6,
+    },
+    SP_1: {
+        TYPE: "SP_1",
+        DMG: 10,
+    },
+    SP_2: {
+        TYPE: "SP_2",
+        DMG: 7,
+    },
+    JUMP: {
+        TYPE: "JUMP",
+        DMG: 6,
+    }
+}//end ATTACK_TYPE  
