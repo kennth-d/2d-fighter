4c283e08e24339d8fb69ca13bf2348e1a74bd265
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-16 16:33:26 -0400

    Aggregates the Scene classes into a single export.  Scenes should be imported through scenes.js

---
+++ src/scripts/scenes/scenes.js
@@ -0,0 +1,10 @@
+import { Scene } from "./Scene.js";
+import { MenuScene } from "./MenuScene.js";
+
+import { MainMenuScene } from "./MainMenuScene.js";
+import { CharacterSelectScene } from "./CharacterSelectScene.js";
+import { SettingsScene } from "./SettingsScene.js";
+import { HowToScene } from "./HowToScene.js";
+import { BattleScene } from "./BattleScene.js";
+
+export { Scene, MenuScene, MainMenuScene, CharacterSelectScene, SettingsScene, HowToScene, BattleScene };
