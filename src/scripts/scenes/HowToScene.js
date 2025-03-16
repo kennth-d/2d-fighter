362ddd3688b1ef2bee600e63d8d0376b4b6b61e6
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-16 16:29:32 -0400

    HowToScene - explains how to play the game.

---
+++ src/scripts/scenes/HowToScene.js
@@ -0,0 +1,40 @@
+import { BUTTONS } from "../../assets/data/buttonsData.js";
+import { Button } from "../form/Button.js";
+import { HOW_TO_BACKGROUND } from "../../assets/data/HowToImgData.js";
+import { BUTTON_WIDTH } from "../../assets/data/buttonsData.js";
+import { MenuScene } from "./scenes.js"
+
+export class HowToScene extends MenuScene {
+    constructor(game) {
+        super(game);
+        this.buttons = [new Button(BUTTONS.RETURN, "RETURN")];
+        
+        //set button position
+        this.buttons[0].rect.x = this.game.ctxHigh.canvas.width - BUTTON_WIDTH - 50;
+        this.buttons[0].rect.y = 130;
+    }//end ctor
+    draw() {
+        this.drawBackground(this.game.ctxHigh);
+        super.draw(this.game.ctxHigh);
+    }//end draw
+    drawBackground(ctx) {
+
+        //scale 1920 x 1080 image down to canvas size of 1280 x 720
+        ctx.scale(.67, .67); // (1280/1920, 720/1080) 
+
+        ctx.drawImage(
+            HOW_TO_BACKGROUND.img,
+            HOW_TO_BACKGROUND.x,
+            HOW_TO_BACKGROUND.y,
+            HOW_TO_BACKGROUND.width,
+            HOW_TO_BACKGROUND.height,
+            0,
+            0,
+            HOW_TO_BACKGROUND.width,
+            HOW_TO_BACKGROUND.height,
+        )//end drawbg
+
+        //reset ctx
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }//end drawBackground
+}//end HowToScene cls
