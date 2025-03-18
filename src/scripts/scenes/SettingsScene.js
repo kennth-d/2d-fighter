024c64e0a7b127c49c037b9fc831fa89d02daa06
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-18 07:58:31 -0400

    added RangeInputs and event handling logic

--- src/scripts/scenes/SettingsScene.js
+++ src/scripts/scenes/SettingsScene.js
@@ -1,53 +1,80 @@
-import { Button, BUTTON_STATES } from "../form/Button.js";
-import { BUTTON_HEIGHT, BUTTON_PADDING, BUTTON_WIDTH, BUTTONS } from "../../assets/data/buttonsData.js";
+import { Button, HoverStatus } from "../form/Button.js";
+import { BUTTON_PADDING, BUTTON_WIDTH, BUTTONS } from "../../assets/data/buttonsData.js";
 import { isInside } from "../utils/mouseHandler.js";
 import { RoundChoiceForm } from "../form/RoundChoiceForm.js";
 import { BUTTON_CALLBACKS } from "../form/ButtonCallbacks.js";
 import { getClickedObject } from "../utils/getClickedObject.js";
 import { MenuScene } from "./scenes.js";
 import { MusicChoiceForm } from "../form/MusicChoiceForm.js";
+import { RangeInput } from "../form/RangeInput.js";
+import { DEFAULT_SETTINGS } from "../utils/global.js";
 
 export class SettingsScene extends MenuScene {
     constructor(game) {
         super(game)
         this.buttons = this.getButtons();
-        this.settings = [
+        this.settingsChoices = [
             new RoundChoiceForm(497, 250),
-            new MusicChoiceForm(586, 500),
-        ]
-        this.innitSettings();
-    }
+            new MusicChoiceForm(586, 475),
+        ];
+        this.rangeSettings = [
+            new RangeInput(540, 350, 200, 20, 100, 300, game.gameSettings.roundDuration, "ROUND_LENGTH"),
+            new RangeInput(540, 425, 200, 20, 0, 100, game.gameSettings.volume, "VOLUME"),
+        ];
+        this.mouseDown = 0;
+    }//end ctor
     handleClickEvent() {
-        for (let i = 0; i < this.settings.length; i++) {
-            let clicked = getClickedObject(this.settings[i].getChoices(), this.mousePos);
+        //handle choices
+        for (let i = 0; i < this.settingsChoices.length; i++) {
+            let clicked = getClickedObject(this.settingsChoices[i].getChoices(), this.mousePos);
             if (clicked) {
-                this.settings[i].setSelection(clicked.id);
+                this.settingsChoices[i].setSelection(clicked.id);
             }
         }//end for
 
-        let clickedBtn = getClickedObject(this.buttons, this.mousePos);
-        if (clickedBtn) BUTTON_CALLBACKS[clickedBtn.id](this.game);
+        //handle buttons
+        super.handleClickEvent();
     }//end handleClickEvent
-    update() {
-        for (const button of this.buttons) {
-            let state = BUTTON_STATES[Number(isInside(button.rect, this.mousePos))];
-            button.update(state);
-        }
-    }//end
+    handleMouseMove(event) {
+        super.handleMouseMove(event);
+
+        if (!this.mouseDown) return;
+        let mouseDownOnRange = getClickedObject(this.rangeSettings, this.mousePos);
+
+        //if mouse is down on range control update and redraw.
+        if (mouseDownOnRange) {
+            mouseDownOnRange.setValue(mouseDownOnRange.getValueFromMouse(this.mousePos));
+            this.draw();
+        }//end if
+    }
+    handleMouseDown() {
+        this.mouseDown++;
+    }
+    handleMouseUp() {
+        this.mouseDown--;
+    }
     draw() {
-        this.drawstub();
+        this.drawbg(this.game.ctxHigh);
         this.drawButtons(this.game.ctxHigh);
+        this.drawChoices(this.game.ctxHigh);
+        this.drawRanges(this.game.ctxHigh);
     }//end draw
     drawButtons(ctx) {
         for (const button of this.buttons) {
             button.draw(ctx);
         }//end for
-
-        for (const setting of this.settings) {
+        ctx.restore();
+    }//end draw buttons
+    drawChoices(ctx) {
+        for (const setting of this.settingsChoices) {
             setting.draw(ctx);
         }//end for
-
-    }//end draw buttons
+    }//end drawChoices
+    drawRanges(ctx) {
+        for (const rangeControl of this.rangeSettings) {
+            rangeControl.draw(ctx);
+        }//end drawranges
+    }//end drawRanges
     getButtons() {
         let returnBtn = new Button(BUTTONS.RETURN, "RETURN");
         returnBtn.setRectProperty("x", this.game.ctxHigh.canvas.width/2 + BUTTON_PADDING);
@@ -58,11 +85,7 @@ export class SettingsScene extends MenuScene {
         confirmBtn.setRectProperty("y",  580);
         return [returnBtn, confirmBtn];
     }//end getButtons
-    innitSettings() {
-        this.settings[0].setSelection(this.game.gameSettings.rounds);
-    }
-    drawstub() {
-        let ctx = this.game.ctxHigh;
+    drawbg(ctx) {
         ctx.fillStyle = "gray";
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
@@ -73,11 +96,5 @@ export class SettingsScene extends MenuScene {
         let dx = ctx.canvas.width/2 - metrics.width/2;
         let dy = ctx.canvas.height/2 - 200;
         ctx.fillText(text, dx, dy);
-
-        ctx.beginPath();
-        ctx.moveTo(ctx.canvas.width/2, 0);
-        ctx.lineTo(ctx.canvas.width/2, ctx.canvas.height);
-        ctx.stroke();
     }//end draw
-    
-}
+}//end SettingsScene
