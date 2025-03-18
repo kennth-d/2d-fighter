9a4ed3446566964cb658e41cfdac717a68f56590
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-18 07:57:03 -0400

    added handleMouseMove to update mouse pos.

--- src/scripts/scenes/MenuScene.js
+++ src/scripts/scenes/MenuScene.js
@@ -1,8 +1,9 @@
 import { Scene } from "./Scene.js"
-import { BUTTON_STATES } from "../form/Button.js";
+import { HoverStatus } from "../form/Button.js";
 import { isInside } from "../utils/mouseHandler.js";
 import { getClickedObject } from "../utils/getClickedObject.js";
 import { BUTTON_CALLBACKS } from "../form/ButtonCallbacks.js";
+import { getMousePos } from "../utils/mouseHandler.js";
 
 export class MenuScene extends Scene {
     constructor(game) {
@@ -11,7 +12,7 @@ export class MenuScene extends Scene {
     }//end ctor
     update() {
         for (const button of this.buttons) {
-            let state = BUTTON_STATES[Number(isInside(button.rect, this.mousePos))];
+            let state = HoverStatus[Number(isInside(button.rect, this.mousePos))];
             button.update(state);
         }//end for
     }//end update
@@ -27,5 +28,11 @@ export class MenuScene extends Scene {
         let clickedBtn = getClickedObject(this.buttons, this.mousePos);
         if (clickedBtn) BUTTON_CALLBACKS[clickedBtn.id](this.game);
     }
+    handleMouseMove(event) {
+        let mPos = getMousePos(this.game.ctxHigh.canvas, event);
+        this.mousePos.x = mPos.x;
+        this.mousePos.y = mPos.y;
+    }//end handleMouseMove
+
 }//end MenuScene cls
     
