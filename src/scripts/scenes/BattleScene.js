66ba53cd78d2ff7f8ef072ce06e313ef216c36dd
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-18 09:30:04 -0400

    BattleScene now extends Scene. constructor now calls createFighter() in  getFighterEntites to get instances of Fighters.

--- src/scripts/scenes/BattleScene.js
+++ src/scripts/scenes/BattleScene.js
@@ -1,64 +1,65 @@
+import {createFighter} from "../fighters/fighters.js";
 import { Stage } from "../stage/Stage.js";
-import { Fighter_001 } from "../fighters/Fighter_001.js";
-import { Fighter_002 } from "../fighters/Fighter_002.js";
-import { PLAYER_ONE_START, PLAYER_TWO_START } from "../utils/global.js";
-import { KeyboardInputComponent } from "../components/KeyboardInputComponent.js";
 import { StatusBar } from "../overlays/StatusBar.js";
+import { Scene } from "./Scene.js";
 
-export class BattleScene {
+export class BattleScene extends Scene {
 
     constructor(game) {
-        this.game = game;
-        this.fighters = this.getFighterEntities();
+        super(game);
+        this.fighters = this.getFighterEntities(this.game.fighters[0], this.game.fighters[1]);
         this.stage = new Stage();
         this.overlays = [
-            new StatusBar(this.fighters),
+            this.getStatusBar(this.fighters, game.gameSettings.rounds, game.gameSettings.roundDuration),
         ];
+
+        if (game.debug) {
+            console.log("DEBBUGIN");
+            game.setupDebug();
+        }//end if
     }
-    getFighterEntities() {
+    getStatusBar(fighters, rounds, duration) {
+        return new StatusBar(fighters, rounds, duration);
+    }//end getStatusBar
+    getFighterEntities(p1, p2) {
         const fighterEntities = [
-                new Fighter_001(PLAYER_ONE_START.x, PLAYER_ONE_START.y, 0, new KeyboardInputComponent()),
-                new Fighter_002(PLAYER_TWO_START.x, PLAYER_TWO_START.y, 1, new KeyboardInputComponent()),
-            ]
+                createFighter(0, p1, 0),
+                createFighter(1, p2, 0),
+            ];
+
             fighterEntities[0].opponent = fighterEntities[1];
             fighterEntities[1].opponent = fighterEntities[0];
 
             return fighterEntities;
-    }
-
+    }//end getFighterEntitiess
     update() {
         this.updateFighters();
         this.stage.update();
         this.updateOverlays();
-    }
-
+    }//end update
     draw() {
         this.stage.draw(this.game.ctx);
         this.drawFighters(this.game.ctx);
         this.drawOverlays(this.game.ctx);
-    }
-
+    }//end draw
     updateFighters() {
         for (const fighter of this.fighters) {
             fighter.update();
         }
-    }
-
+    }//end updateFighters
     updateOverlays() {
         for (const overlay of this.overlays) {
             overlay.update();
         }
-    }
-
+    }//end updateOverlays
     drawFighters(ctx) {
         for (const fighter of this.fighters) {
             fighter.draw(ctx);
         }
-    }
-
+    }//end drawFighters
     drawOverlays(ctx) {
         for (const overlay of this.overlays) {
             overlay.draw(ctx);
         }
-    }
+    }//end drawOverlays
 }
