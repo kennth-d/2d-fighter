00dad61da487b8819cf0bb5f1f80e9191b8350f9
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-19 20:58:57 -0400

    added import/export for PauseScene.

--- src/scripts/scenes/scenes.js
+++ src/scripts/scenes/scenes.js
@@ -5,7 +5,7 @@ import { MainMenuScene } from "./MainMenuScene.js";
 import { CharacterSelectScene } from "./CharacterSelectScene.js";
 import { SettingsScene } from "./SettingsScene.js";
 import { HowToScene } from "./HowToScene.js";
-//import { PauseScene } from "./PauseScene.js";
+import { PauseScene } from "./PauseScene.js";
 import { BattleScene } from "./BattleScene.js";
 
-export { Scene, MenuScene, MainMenuScene, CharacterSelectScene, SettingsScene, HowToScene, BattleScene };
+export { Scene, MenuScene, MainMenuScene, CharacterSelectScene, SettingsScene, HowToScene, BattleScene, PauseScene };
