71b73f50987677566a0a135ec6ebff1d6796ba9c
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-25 12:52:27 -0400

    helper function to retrieve the box data for a specific fighter, boxtype, animation and frame.

---
+++ src/scripts/utils/getBoxes.js
@@ -0,0 +1,14 @@
+import * as boxes from "../../assets/data/boxData.js";
+
+/** gets box data for a Fighter's current state and animation frame.
+ * 
+ * @param {string} fighter Fighter.name
+ * @param {string} state current state/animation of the fighter.
+ * @param {string} boxType push, hurt or hit.
+ * @param {int} frame current frame of the fighter's animation.
+ * @returns [x, y, w, h] for push and hit boxes. [[x, y, w, h],[x, y, w, h],[x, y, w, h]] for hurt boxes.
+ */
+export function getBoxes(fighter, state, boxType, frame) {
+    //console.log(fighter, frame, ":  ", boxes[fighter][boxType][state][frame]);
+    return boxes[fighter][boxType][state][frame];
+}//end getBoxes
