6dd8ba556d029d56f4238659c4d501cdb8d4cf00
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-25 12:49:22 -0400

    - changed correctDirection to use fighter.pos instead of origin.

--- src/scripts/utils/correctDirection.js
+++ src/scripts/utils/correctDirection.js
@@ -2,9 +2,10 @@ import { OpponentDirection } from "./global.js";
 
 //changes fighters direction when they cross over the opponent.
 export function correctDirection(fighter, opponent) {
-    if (fighter.origin.x > opponent.pushBox.x + opponent.pushBox.width) {
+    let opponentPushX = opponent.boxes.push[0];
+    if (fighter.pos.x > opponent.pos.x + opponentPushX) {
         fighter.direction = OpponentDirection.LEFT;
-    } else if (fighter.origin.x < opponent.pushBox.x) {
+    } else if (fighter.pos.x < opponent.pos.x - opponentPushX ) {
         fighter.direction = OpponentDirection.RIGHT;
     }//end if-else
 }// end getDirection
