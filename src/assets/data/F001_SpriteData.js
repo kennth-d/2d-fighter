b67a9ca0b181949ddb97735dd95720b34ceb992d
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-25 12:36:14 -0400

    - added const path for easier imports, updated originOffsets, removed pushboxes.

--- src/assets/data/F001_SpriteData.js
+++ src/assets/data/F001_SpriteData.js
@@ -1,138 +1,130 @@
 //** All data for F001 sprites is in this file.
-// default image size is 48x48.
 // format:
 // STATE: {
 //          src: the image source
 //          width: frame width
 //          height: frame height
 //          frames: total number of frames in the image
-//          originOffset: {x:(thisWidth - 48), y:(thisHeight - 40)}
-//          delay: (n) slows the animation down by n.
-//          pushBox: {width, height}  Default values: {15, 30}
+//          originOffset: used to center the image on the canvas context.
+//          delay: (n) slows the animation down by a factor of n.
 // */
+
+const path = "../src/assets/images/fighters/F001/F001 ";
 
 export const F001_SpriteData = {
     IDLE: {
-        src :"../src/assets/images/fighters/F001/F001 idle 48x48.png",
+        src : path + "idle 48x48.png",
         width: 48,
         height: 48,
         frames: 10,
-        originOffset: {x: 0, y: 8},
-        delay: 1.5,
+        originOffset: {x: 24, y: 40},
     },
     BLOCK: {
-        src: "../src/assets/images/fighters/F001/F001 light attack 48x48.png",
+        src: path + "idle 48x48.png",
         width: 48,
         height: 48,
         frames: 1,
-        originOffset: {x: 0, y: 8},
+        originOffset: {x: 24, y: 40},
     },
     WALK_FWD: {
-        src: "../src/assets/images/fighters/F001/F001 walk fwd 48x48.png",
+        src: path + "walk fwd 48x48.png",
         width: 48,
         height: 48,
         frames: 8,
-        originOffset: {x: 0, y: 8},
+        originOffset: {x: 24, y: 40},
         delay: 1.05,
+
     },
     WALK_BWD: {
-        src: "../src/assets/images/fighters/F001/F001 walk bwd 48x48.png",
+        src: path + "walk bwd 48x48.png",
         width: 48,
         height: 48,
         frames: 8,
-        originOffset: {x: 0, y: 8},
+        originOffset: {x: 24, y: 40},
         delay: 1.05,
     },
-    DASH: {
-        src: "../src/assets/images/fighters/F001/F001 dash 48x48.png",
-        width: 48,
-        height: 48,
-        frames: 9,
-        originOffset: {x: 0, y: 8},
-    },
     CROUCH: {
-        src: "../src/assets/images/fighters/F001/F001 crouch 48x48.png",
+        src: path + "crouch 48x48.png",
         width: 48,
         height: 48,
         frames: 10,
-        originOffset: {x: 0, y: 8},
-        pushBox: {width: 20, height: 20}
+        originOffset: {x: 24, y: 40},
     },
-    // CROUCH_ATTACK: {
-    //     src: undefined,
-    //     width: undefined,
-    //     height: undefined,
-    //     frames: undefined,
-    // },
+
     LIGHT_ATTACK: {
-        src: "../src/assets/images/fighters/F001/F001 light attack 48x48.png",
+        src: path + "light attack 48x48.png",
         width: 48,
         height: 48,
         frames: 10,
-        originOffset: {x: 0, y: 8},
+        originOffset: {x: 22, y: 40},
     },
     HEAVY_ATTACK: {
-        src: "../src/assets/images/fighters/F001/F001 heavy attack 64x48.png",
+        src: path + "heavy attack 64x48.png",
         width: 65,
         height: 48,
         frames: 7,
-        originOffset: {x: 17, y: 8},
+        originOffset: {x: 24, y: 40},
         delay: 2,
     },
     SP_1: {
-        src: "../src/assets/images/fighters/F001/F001 special 1 80x48.png",
+        src: path + "special 1 80x48.png",
         width: 80,
         height: 64,
-        frames: 10
+        frames: 10,
+        originOffset: {x: 37, y: 40},
+        delay: 2,
     },
     SP_2: {
-        src: "../src/assets/images/fighters/F001/F001 special 2 48x48.png",
+        src: path + "special 2 48x48.png",
         width: 48,
         height: 48,
-        frames: 10
+        frames: 10,
+        originOffset: {x: 23, y: 40},
+        delay: 1.5,
     },
     JUMP: {
-        src: "../src/assets/images/fighters/F001/F001 jump 48x48.png",
+        src: path + "jump 48x48.png",
         width: 48,
         height: 48,
         frames: 6,
-        originOffset: {x: 0, y: 8},
+        originOffset: {x: 21, y: 40},
         delay: 2,
     },
     JUMP_FWD: {
-        src: "../src/assets/images/fighters/F001/F001 jump 48x48.png",
+        src: path + "jump 48x48.png",
         width: 48,
         height: 48,
         frames: 6,
-        originOffset: {x: 0, y: 8},
-        delay: 2,
+        originOffset: {x: 21, y: 40},
+        delay: 1.5,
     },
     JUMP_BWD: {
-        src: "../src/assets/images/fighters/F001/F001 jump bwd 48x48.png",
+        src: path + "jump bwd 48x48.png",
         width: 48,
         height: 48,
         frames: 6,
-        originOffset: {x: 0, y: 8},
+        originOffset: {x: 27, y: 40},
         delay: 2,
     },
     HURT: {
-        src: "../src/assets/images/fighters/F001/F001 hurt 48x48.png",
+        src: path + "hurt 48x48.png",
         width: 48,
         height: 48,
-        originOffset: {x: 0, y: 8},
-        frames: 4
+        originOffset: {x: 22, y: 40},
+        frames: 4,
     },
     KNOCKBACK: {
-        src: "../src/assets/images/fighters/F001/F001 knockback 48x48.png",
+        src: path + "knockback 48x48.png",
         width: 48,
         height: 48,
-        originOffset: {x: 0, y: 8},
-        frames: 7
+        frames: 7,
+        originOffset: {x: 24, y: 40},
     },
-    DEATH: {
-        src: "../src/assets/images/fighters/F001/F001 death 64x64.png",
+    KO: {
+        src: path + "death 48x48.png",
         width: 64,
         height: 64,
-        frames: 10
+        frames: 10,
+        originOffset: {x: 24, y: 40},
     },
 }//end F_001 
