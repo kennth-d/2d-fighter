de12da3328bc15c8b3dbb580d5cada37eef4267b
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-27 15:04:11 -0400

    -added comments  and made it so direction changes based soley on x position.

--- src/scripts/utils/correctDirection.js
+++ src/scripts/utils/correctDirection.js
@@ -1,12 +1,14 @@
-import { OpponentDirection } from "./global.js";
-
-//changes fighters direction when they cross over the opponent.
+import { OPPONENT_DIRECTION } from "./const.js";
+import { FighterBaseClass } from "../fighters/FighterBaseClass.js"
+/** Flips the direction property of a Fighter when
+ *  the fighter crosses its opponent on the x-axis.
+ * @param {FighterBaseClass} fighter 
+ * @param {FighterBaseClass} opponent 
+ */
 export function correctDirection(fighter, opponent) {
-    let opponentPushX = opponent.boxes.push[0];
-    if (fighter.pos.x > opponent.pos.x + opponentPushX) {
-        fighter.direction = OpponentDirection.LEFT;
-    } else if (fighter.pos.x < opponent.pos.x - opponentPushX ) {
-        fighter.direction = OpponentDirection.RIGHT;
+    if (fighter.pos.x > opponent.pos.x) {
+        fighter.direction = OPPONENT_DIRECTION.LEFT;
+    } else if (fighter.pos.x < opponent.pos.x) {
+        fighter.direction = OPPONENT_DIRECTION.RIGHT;
     }//end if-else
 }// end getDirection
-
