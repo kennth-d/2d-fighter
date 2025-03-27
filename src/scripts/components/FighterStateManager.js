7148fae18d5e89568039a36efa562471ca7b3226
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-27 14:50:13 -0400

    changed to use states.js module for importing states, added exit() method call before changing state.

--- src/scripts/components/FighterStateManager.js
+++ src/scripts/components/FighterStateManager.js
@@ -1,26 +1,26 @@
-import { Idle, Crouch, WalkFwd, WalkBwd, Jump, LightAttack, HeavyAttack, JumpForward, JumpBack, SP_1, SP_2 } from "../states/states.js";
+import * as states from "../states/states.js";
 
 export class FighterStateManager {
     constructor(fighter) {
         this.states = {
-            IDLE: new Idle(),
-            CROUCH: new Crouch(),
-            WALK_FWD: new WalkFwd(),
-            WALK_BWD: new WalkBwd(),
-            JUMP: new Jump(),
-            JUMP_FWD: new JumpForward(),
-            JUMP_BWD: new JumpBack(),
-            LIGHT_ATTACK: new LightAttack(),
-            HEAVY_ATTACK: new HeavyAttack(),
-            SP_1: new SP_1(),
-            SP_2: new SP_2(),
+            IDLE: new states.Idle(),
+            CROUCH: new states.Crouch(),
+            WALK_FWD: new states.WalkFwd(),
+            WALK_BWD: new states.WalkBwd(),
+            JUMP: new states.Jump(),
+            JUMP_FWD: new states.JumpForward(),
+            JUMP_BWD: new states.JumpBack(),
+            LIGHT_ATTACK: new states.LightAttack(),
+            HEAVY_ATTACK: new states.HeavyAttack(),
+            SP_1: new states.SP_1(),
+            SP_2: new states.SP_2(),
         }//end states
         this.activeState = this.states.IDLE;
         this.fighter = fighter;
     }//end ctor
     transition(newState) {
+        this.activeState.exit(this);
         this.activeState = this.states[newState];
         this.activeState.enter(this);
-        
-    }//end changeState
-}//end cls
+    }//end transition
+}//end FighterStateManger.
