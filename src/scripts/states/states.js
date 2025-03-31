fd206722c47574fa3eefc3ff2e1715bdeb4697c6
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-31 18:07:47 -0400

    added hurt.js

--- src/scripts/states/states.js
+++ src/scripts/states/states.js
@@ -10,14 +10,16 @@
  * @exports HeavyAttack,
  * @exports SP_1,
  * @exports SP_2,
+ * @exports Hurt
  * @exports characterStates a list of all the states as strings.
  * 
  */ 
 
 import { Idle, WalkFwd, WalkBwd, Jump, JumpBack, JumpForward, Crouch } from "./MoveStates.js";
 import { LightAttack, HeavyAttack, SP_1, SP_2 } from "./AttackStates.js";
+import { Hurt } from "./HurtStates.js";
 
-export const characterStates = ["IDLE", "CROUCH", "WALK_FWD", "WALK_BWD", "JUMP", "LIGHT_ATTACK", "HEAVY_ATTACK", "JUMP_FWD", "JUMP_BWD", "SP_1", "SP_2"];
+export const characterStates = ["IDLE", "CROUCH", "WALK_FWD", "WALK_BWD", "JUMP", "LIGHT_ATTACK", "HEAVY_ATTACK", "JUMP_FWD", "JUMP_BWD", "SP_1", "SP_2", "HURT"];
 
 export {
     Idle,
@@ -31,4 +33,5 @@ export {
     HeavyAttack,
     SP_1,
     SP_2,
+    Hurt,
 };
