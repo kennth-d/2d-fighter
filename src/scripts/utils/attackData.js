326c235e3b723b2964e6dc64b53f2a78036b9b0e
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-31 18:08:15 -0400

    stores dmg, hitstun and knockback for attacks

---
+++ src/scripts/utils/attackData.js
@@ -0,0 +1,23 @@
+const LIGHT_ATTACK = {
+    dmg: 5,
+    hitstun: .5,
+    knockback: 150,
+};
+const HEAVY_ATTACK = {
+    dmg: 10,
+    hitstun: .7,
+    knockback: 200,
+};
+const SP_1 = {
+    dmg: 15,
+    hitstun: .7,
+    knockback: 500,
+};
+const SP_2 = {
+    dmg: 7,
+    hitstun: .3,
+    knockback: 100,
+};
+
+const HIT_STOP = .15;
+export { LIGHT_ATTACK, HEAVY_ATTACK, SP_1, SP_2, HIT_STOP };
