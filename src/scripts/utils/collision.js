a92940de4659705060b234765e4d7da3d42bbddd
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-03-31 18:08:52 -0400

    added updateHitBoxCollision

--- src/scripts/utils/collision.js
+++ src/scripts/utils/collision.js
@@ -1,35 +1,28 @@
-import { BOUNDARIES } from "./const.js";
+import { BOUNDARIES, TIME } from "./const.js";
+import { getActualBoxDimensions } from "./utils.js";
+import { applyhit } from "./hit.js";
+import { HIT_STOP } from "./attackData.js";
 
 /** Checks if two rectangles overlap.
- * @param {{x, y, width, height}} boxA an object containing x, y, width, height properties
- * @param {{x, y, width, height}} boxB an object containing x, y, width, height prooperties
+ * @param {{x:number, y: number, width: number, height: number}} boxA an object containing x, y, width, height properties
+ * @param {{x: number, y: number, width: number, height:number}} boxB an object containing x, y, width, height prooperties
  * @returns {boolean} true the boxes overlap, false otherwise.
  **/
 export function rectsOverlap(boxA, boxB) {
-    return boxA.x < boxB.x + boxB.width &&
-           boxA.x + boxA.width > boxB.x &&
-           boxA.y < boxB.y + boxB.height &&
-           boxA.y + boxA.height > boxB.y;
-}//end rectsOverlap
+    return Math.max(boxA.x, boxB.x) < Math.min(boxA.x + boxA.width, boxB.x + boxB.width) &&
+           Math.max(boxA.y, boxB.y) < Math.min(boxA.y + boxA.height, boxB.y + boxB.height)
+
+}
 /** Pushes players away from eachother to prevent overlap.
 *   @param {Fighter[]} players an array of Fighter objects.
 **/
-export function resolvePlayerCollision(players) {
+export function resolvePushBoxCollision(players) {
         //players 1
-        const boxA = {
-            x:      players[0].pos.x + players[0].boxes.push[0],
-            y:      players[0].pos.y + players[0].boxes.push[3],
-            width:  players[0].boxes.push[2],
-            height: players[0].boxes.push[3],
-        };
-        //players 2
-        const boxB = {
-            x:      players[1].pos.x + players[1].boxes.push[0],
-            y:      players[1].pos.y + players[1].boxes.push[3],
-            width:  players[1].boxes.push[2],
-            height: players[1].boxes.push[3],
-        };
+        const boxA = getActualBoxDimensions(players[0].pos, players[0].direction, players[0].boxes.push);
 
+        //player 2
+        const boxB = getActualBoxDimensions(players[1].pos, players[1].direction, players[1].boxes.push);
+  
         if (!rectsOverlap(boxA, boxB)) return;
 
         let xOverlap = Math.min(
@@ -80,3 +73,33 @@ export function ensureOnFLoor(fighter) {
     }//end if
 }//end ensureOnFloor
 
+export function updateHitBoxCollision(scene, attacker, opponent) {
+     if (!attacker.isAttacking() || attacker.getHasHit()) return;
+     let frame = attacker.spriteManager.currentFrame+1
+     let attack = attacker.stateManager.activeState.name
+
+     const trueHitBox = getActualBoxDimensions(attacker.pos, attacker.direction, attacker.boxes.hit);
+     for (const box of opponent.boxes.hurt) {
+        const trueOpponentHurtBox = getActualBoxDimensions(opponent.pos, opponent.direction, box);
+    
+        if (!rectsOverlap(trueHitBox, trueOpponentHurtBox)) continue;
+
+        let bodyparts = ["head", "body", "legs"]
+        const hurtIndex = opponent.boxes.hurt.indexOf(box);
+        console.log(`${attacker.name} has hit ${opponent.name}'s ${bodyparts[hurtIndex]} with frame: ${frame}`);
+        attacker.setHasHit(true);
+        applyhit(opponent, attack);
+        TIME.hitStopTimer = TIME.previous + (HIT_STOP * 1000);
+        scene.drawOrder = [opponent.playerId, attacker.playerId];
+
+        const hitLocation = {
+            x: (trueHitBox.x + (trueHitBox.width / 2) + trueOpponentHurtBox.x + (trueOpponentHurtBox.width / 2)) / 2,
+            y: (trueHitBox.y + (trueHitBox.height / 2) + trueOpponentHurtBox.y + (trueOpponentHurtBox.height / 2)) / 2,
+        }
+        hitLocation.x -= 4 - Math.random() * 8;
+        hitLocation.y -= 4 - Math.random() * 8;
+        //TODO generate hit sprite
+        //attacker.generateHitSprite(hitLocation);
+        return;
+     }//end for
+}//end checkHitBoxCollision
