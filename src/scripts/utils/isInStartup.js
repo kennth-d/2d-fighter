139cb059193b111ea82ac7d53f01a0cd06ab1d8d
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-02 15:01:30 -0400

    added to help determine if block state should be entered. block state will be entered if input away from opponent is held + reviever is in range of attack + attacker is in startup of the attack.

---
+++ src/scripts/utils/isInStartup.js
@@ -0,0 +1,15 @@
+import { FighterBaseClass } from "../fighters/FighterBaseClass.js";
+import * as boxes from "../../assets/data/boxData.js"
+/**checks if a Figher is in the startup of an attack.
+ * Startup is defined as the animation frames before the attack has a hitbox.
+ * @param { FighterBaseClass } fighter an instance of FighterBaseClass 
+ * @returns { Boolean } true if fighter is in startup false otherwise
+ */
+export function isInStartup(fighter) {
+    if (!fighter.isAttacking()) return;
+    let attack = fighter.stateManager.activeState.name;
+    let hitboxes = boxes[fighter.name].hit[attack];
+    let idx = hitboxes.findIndex(arr => Array.isArray(arr) && arr.length > 0);
+    let currentFrame = fighter.spriteManager.currentFrame;
+    return currentFrame < idx + 1;
+}
