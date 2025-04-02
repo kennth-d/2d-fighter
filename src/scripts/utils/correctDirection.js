aa603674e4650f5a6da44eede9478a8c170f93c2
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-02 14:54:54 -0400

    added a call to isAtBoundary to prevent players from switching directions when the opponent is backed against a boundary.

--- src/scripts/utils/correctDirection.js
+++ src/scripts/utils/correctDirection.js
@@ -1,15 +1,21 @@
 import { OPPONENT_DIRECTION } from "./const.js";
-import { FighterBaseClass } from "../fighters/FighterBaseClass.js"
+import { FighterBaseClass } from "../fighters/FighterBaseClass.js";
+import { isAtBoundary } from "./collision.js";
 /** Flips the direction property of a Fighter when
  *  the fighter crosses its opponent on the x-axis.
  * @param {FighterBaseClass} fighter 
  * @param {FighterBaseClass} opponent 
  */
-export function correctDirection(fighter, opponent) {
-    if (fighter.isAttacking()) return;
-    if (fighter.pos.x > opponent.pos.x) {
-        fighter.direction = OPPONENT_DIRECTION.LEFT;
-    } else if (fighter.pos.x < opponent.pos.x) {
-        fighter.direction = OPPONENT_DIRECTION.RIGHT;
+export function correctDirection(p1, p2) {
+    if (isAtBoundary(p1) || isAtBoundary(p2)) {
+        return;
+    }
+
+    if (p1.pos.x > p2.pos.x) {
+        p1.direction = OPPONENT_DIRECTION.LEFT;
+        p2.direction = OPPONENT_DIRECTION.RIGHT;
+    } else if (p1.pos.x < p2.pos.x) {
+        p1.direction = OPPONENT_DIRECTION.RIGHT;
+        p2.direction = OPPONENT_DIRECTION.LEFT;
     }//end if-else
 }// end getDirection
