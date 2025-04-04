58096907d38649c5146194fe3f73ff81c842707b
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-04 09:12:46 -0400

    updated to reflect new image

--- src/assets/data/HowToImgData.js
+++ src/assets/data/HowToImgData.js
@@ -2,6 +2,6 @@ export const HOW_TO_BACKGROUND = {
     img: document.querySelector('img[alt="how-to-bg"]'),
     x: 0,
     y: 0,
-    width: 1920,
-    height: 1080,
+    width: 850,
+    height: 850,
 }
