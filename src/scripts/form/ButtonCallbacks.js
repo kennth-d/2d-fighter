344177720f3fa939e38f2b202a502b4f35052b22
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-06 18:11:59 -0400

    -updated button callbacks to use game manager methods.

--- src/scripts/form/ButtonCallbacks.js
+++ src/scripts/form/ButtonCallbacks.js
@@ -2,46 +2,31 @@ import * as scenes from "../scenes/scenes.js";
 
 export const BUTTON_CALLBACKS = {
     "PLAY": (game) => {
-        game.scenes.push(new scenes.CharacterSelectScene(game));
-        game.numScenes += 1;
-        game.scene = game.scenes[game.numScenes - 1];
+        game.addScene(new scenes.CharacterSelectScene(game));
     },
     "SETTINGS": (game) => {
-        game.scenes.push(new scenes.SettingsScene(game));
-        game.numScenes += 1;
-        game.scene = game.scenes[game.numScenes - 1]
+        game.addScene(new scenes.SettingsScene(game));
     },
     "HOW": (game) => {
-        game.scenes.push(new scenes.HowToScene(game));
-        game.numScenes += 1;
-        game.scene = game.scenes[game.numScenes - 1];
+        game.addScene(new scenes.HowToScene(game));
     },
     "RETURN": (game) => {      
         game.ctxHigh.clearRect(0, 0, game.ctxHigh.canvas.width, game.ctxHigh.canvas.height);
-        game.scenes.pop();
-        game.numScenes -= 1;
-        game.scene = game.scenes[game.numScenes - 1]
+        game.removeScene();
     },
     "RESUME": (game) => {
         game.ctxHigh.clearRect(0, 0, game.ctxHigh.canvas.width, game.ctxHigh.canvas.height);
-        game.scenes.pop();
-        game.numScenes -= 1;
-        game.scene = game.scenes[game.numScenes - 1]
+        game.removeScene();
     },
     "CONFIRM": (game) => {
-        let scene = game.scenes.pop();
+        const scene = game.removeScene();
 
-        //update number of rounds and music on/off
         for (let setting of scene.settingsChoices) {
             game.gameSettings[setting.id] = setting.getSelection();
         }
 
-        //update roundDuration and volume.
         game.gameSettings.roundDuration = scene.rangeSettings[0].getValue();
         game.gameSettings.volume = scene.rangeSettings[1].getValue();
-        
-        game.numScenes -=1;
-        game.scene = game.scenes[game.numScenes - 1];
     },
     "MAIN": (game) => {
         game.scenes = game.scenes.slice(0, 1);
@@ -49,7 +34,9 @@ export const BUTTON_CALLBACKS = {
         game.scene = game.scenes[game.numScenes-1];
     },
     "BATTLE": (game) => {
-        let selectedFighters = game.scene.selectedCharacters;
+        const scene = game.removeScene();
+
+        let selectedFighters = scene.selectedCharacters;
         if (selectedFighters.length < 2) {
             alert ("PLease select two characters.");
             return;
@@ -57,9 +44,8 @@ export const BUTTON_CALLBACKS = {
         game.fighters[0] = selectedFighters[0].id;
         game.fighters[1] = selectedFighters[1].id; 
         
-        game.scenes.pop();
+
         game.ctxHigh.clearRect(0, 0, game.ctxHigh.canvas.width, game.ctxHigh.canvas.height);
-        game.scenes.push(new scenes.BattleScene(game));
-        game.scene = game.scenes[game.numScenes-1];
+        game.addScene(new scenes.BattleScene(game));
     }
 }
