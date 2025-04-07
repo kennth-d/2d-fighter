e2cf500d4a2d3e912429c6293e7e4e83e20a5d86
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-07 17:42:51 -0400

    added playSound(attack) method to applyhit method.

--- src/scripts/utils/hit.js
+++ src/scripts/utils/hit.js
@@ -1,28 +1,32 @@
 import * as attacks from "./attackData.js";
 import { isAtBoundary } from "../utils/collision.js";
+import { playSound } from "./playSound.js";
 
 /**Applies damage to health if fighter is not blocking or is out of energy,
  * or to energy if the fighter is blocking. chains into applyKnockback().
  * @param { FighterBaseClass } reciever Fighter recieving the attack.
  * @param { String } attack name of the Fighter attack state.
  */
-export function applyhit(reciever, attack) {
-
+export function applyhit(reciever, attack, viewport) {
     let attackData = attacks[attack];
     let damage = attackData.dmg;
     let knockbackX = attackData.knockback;
     let knockbackY = 0;
     
     if (reciever.isBlocking() && reciever.hasEnergy()) {
+        
+        playSound(attacks.SOUNDS["BLOCK"].hit);
         reciever.applyDamage(0, damage * 2);
         knockbackX *= .5;
         reciever.applyBlockStun(attackData.hitstun * .5);
     } else {
+        playSound(attacks.SOUNDS[attack].hit);
         reciever.applyDamage(damage, 0);
         reciever.applyHitStun(attackData.hitstun);
         reciever.stateManager.transition("HURT");
     };
-    applyKnockback(reciever, knockbackX, knockbackY);   
+    
+    applyKnockback(reciever, attack, knockbackX, knockbackY, viewport);   
 };//end applyDamage
 
 /**Applies knockback velocity to a Fighter's physics component. If 
@@ -32,14 +36,13 @@ export function applyhit(reciever, attack) {
  * @param {Number} strengthX strength of the knockback on the x-axis
  * @param {Number} strengthY strength of the knockback on the y-axis
  */
-function applyKnockback(reciever, strengthX, strengthY) {
-    if (!isAtBoundary(reciever)) {
+function applyKnockback(reciever, attack, strengthX, strengthY, viewport) {
+    if (!isAtBoundary(reciever, viewport)) {
         //not at boundary
         reciever.physics.changeKnockback("x", strengthX * reciever.opponent.direction);
         reciever.physics.changeKnockback("y", strengthY);
     } else {
-        //is at a boundary
-        //push opponent in the direction the reciever is facing.
+        if (attack === "SP_2") return;
         reciever.opponent.physics.changeKnockback("x", strengthX * reciever.direction);
     }//end if else
 };//end applyKnockback
