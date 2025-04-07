fa1749f9455bdc7d63929d97fbc8ac5c0cad5b3e
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-07 17:39:28 -0400

    added SOUNDS constant to store hit/swing sounds for attacks.

--- src/scripts/utils/attackData.js
+++ src/scripts/utils/attackData.js
@@ -19,5 +19,26 @@ const SP_2 = {
     knockback: 100,
 };
 
+const SOUNDS = {
+    LIGHT_ATTACK: {
+        swing: document.querySelector("audio#light-swing"),
+        hit: document.querySelector("audio#light-hit"),
+    },
+    HEAVY_ATTACK: {
+        swing: document.querySelector("audio#heavy-swing"),
+        hit: document.querySelector("audio#heavy-hit"),
+    },
+    SP_1: {
+        swing: document.querySelector("audio#sp-1-swing"),
+        hit: document.querySelector("audio#sp-1-hit"),
+    },
+    SP_2: {
+        swing: document.querySelector("audio#sp-2-swing"),
+        hit: document.querySelector("audio#sp-2-hit"),
+    },
+    BLOCK: {
+        hit: document.querySelector("audio#block"),
+    },
+};
 const HIT_STOP = .15;
-export { LIGHT_ATTACK, HEAVY_ATTACK, SP_1, SP_2, HIT_STOP };
+export { LIGHT_ATTACK, HEAVY_ATTACK, SP_1, SP_2, HIT_STOP, SOUNDS };
