39cb1d1b9e9fd22c2680eea69ebc4acb5f846aad
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-08 14:05:48 -0400

    -added arial hit function, if reciever is airborne applyhit passes its data to apply aerialhit.

--- src/scripts/utils/hit.js
+++ src/scripts/utils/hit.js
@@ -1,13 +1,20 @@
 import * as attacks from "./attackData.js";
 import { isAtBoundary } from "../utils/collision.js";
 import { playSound } from "./playSound.js";
+import { HURT, KNOCKBACK } from "../states/HurtStates.js";
 
 /**Applies damage to health if fighter is not blocking or is out of energy,
  * or to energy if the fighter is blocking. chains into applyKnockback().
  * @param { FighterBaseClass } reciever Fighter recieving the attack.
  * @param { String } attack name of the Fighter attack state.
  */
-export function applyhit(reciever, attack, viewport) {
+export function applyhit(reciever, attack) {
+
+    if (reciever.physics.isAirborne()) {
+        applyAerialHit(reciever, attack);
+        return;
+    };
+
     let attackData = attacks[attack];
     let damage = attackData.dmg;
     let knockbackX = attackData.knockback;
@@ -26,8 +33,21 @@ export function applyhit(reciever, attack, viewport) {
         reciever.stateManager.transition("HURT");
     };
     
-    applyKnockback(reciever, attack, knockbackX, knockbackY, viewport);   
+    applyKnockback(reciever, attack, knockbackX, knockbackY);   
 };//end applyDamage
+
+function applyAerialHit(reciever, attack) {
+    let attackData = attacks[attack];
+    let damage = attackData.dmg;
+    let knockbackX = attackData.knockback*2;
+    let knockbackY = -150;
+    playSound(attacks.SOUNDS[attack].hit);
+
+    reciever.applyDamage(damage, 0);
+    reciever.applyHitStun(attackData.hitstun);
+    reciever.stateManager.transition("KNOCKBACK");
+    applyKnockback(reciever, attack, knockbackX, knockbackY);
+}//end ApplyArialHit
 
 /**Applies knockback velocity to a Fighter's physics component. If 
  * the reciever is backed against a boundary the knockback force is transfered
@@ -36,13 +56,6 @@ export function applyhit(reciever, attack, viewport) {
  * @param {Number} strengthX strength of the knockback on the x-axis
  * @param {Number} strengthY strength of the knockback on the y-axis
  */
-function applyKnockback(reciever, attack, strengthX, strengthY, viewport) {
-    if (!isAtBoundary(reciever, viewport)) {
-        //not at boundary
-        reciever.physics.changeKnockback("x", strengthX * reciever.opponent.direction);
-        reciever.physics.changeKnockback("y", strengthY);
-    } else {
-        if (attack === "SP_2") return;
-        reciever.opponent.physics.changeKnockback("x", strengthX * reciever.direction);
-    }//end if else
+function applyKnockback(reciever, attack, strengthX, strengthY) {
+     reciever.physics.applyKnockback(strengthX * reciever.opponent.direction, strengthY);
 };//end applyKnockback
