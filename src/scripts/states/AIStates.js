16bd32ce18d328ae3184d3e0b617dca91de49440
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-08 22:29:20 -0400

    add AI states module for managing AI behaviors

---
+++ src/scripts/states/AIStates.js
@@ -0,0 +1,7 @@
+import { OBSERVE } from "./ai/Observe.js";
+import { DEFEND } from "./ai/Defend.js";
+import { ENGAGE } from "./ai/Engage.js";
+import { APPROACH } from "./ai/Approach.js";
+import { RETREAT } from "./ai/Retreat.js";
+
+export { OBSERVE, DEFEND, ENGAGE, APPROACH, RETREAT };
