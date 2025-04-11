a2477029c7b306d5dfc56ec2065ec8d39252b2da
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-11 09:28:55 -0400

    fixed an issue allowing the left player to drag the right player when pushing against the boundary. removed unused deadzone/margin variables, updated draw debug to draw the camera midpoint.

--- src/scripts/components/Viewport.js
+++ src/scripts/components/Viewport.js
@@ -1,42 +1,31 @@
-import { BOUNDARIES, CANVAS_WIDTH, STAGE, CANVAS_HEIGHT } from "../utils/const.js";
+import { BOUNDARIES, CANVAS_WIDTH, STAGE, CANVAS_HEIGHT, CANVAS, MENU } from "../utils/const.js";
 import { lerp } from "../utils/linearInterpolation.js";
 export class Viewport {
     constructor(x, y, fighters) {
         this.fighters = fighters;
         this.pos = {x, y};
-        this.speed = 10;
-        this.margin = 50;
-        this.deadZone = {left: 50, right: CANVAS_WIDTH - 50};
+        this.midX = (this.pos.x + CANVAS_WIDTH-32) / 2;
     }
     update() {
-        //TODO: fix issue of left fighter being able to drag the camera.
-        const leftF = this.fighters[0].pos.x < this.fighters[1].pos.x ? this.fighters[0] : this.fighters[1];
-        const rightF = this.fighters[0].pos.x < this.fighters[1].pos.x ? this.fighters[1] : this.fighters[0];
 
         let targetViewportY = -Math.floor(Math.max(BOUNDARIES.FLOOR - this.fighters[0].pos.y, BOUNDARIES.FLOOR - this.fighters[1].pos.y)/12);
-
-        const midpoint = (this.fighters[0].pos.x + this.fighters[1].pos.x) / 2;
-        let targetViewportX = midpoint - CANVAS_WIDTH / 2;
-
+        
+        const midpoint = (this.fighters[0].pos.x  + this.fighters[1].pos.x ) / 2;
+        let targetViewportX = midpoint - (CANVAS_WIDTH-32) / 2; // -32 to account for camera offset.
+     
         //clamp to stage boundaries
         targetViewportX = Math.max(0, Math.min(targetViewportX, STAGE.WIDTH - CANVAS_WIDTH));
-
+        
         //linear interpolation for smoothing.
         this.pos.x = Math.floor(lerp(this.pos.x, targetViewportX, 0.05)); 
         this.pos.y = Math.floor(lerp(this.pos.y, targetViewportY, 0.6));
-
     }//end update
     drawDebug(ctx) {
         ctx.save();
-        ctx.globalAlpha = 0.1;
-        ctx.fillStyle = "yellow";
-        ctx.fillRect(this.deadZone.left, 0, this.deadZone.right - this.deadZone.left, CANVAS_HEIGHT);
-        ctx.globalAlpha = 1;
-
-        ctx.strokeStyle = "red";
+        ctx.strokeStyle = "blue";
         ctx.lineWidth = 2;
-        ctx.moveTo((this.pos.x + CANVAS_WIDTH/2 - this.pos.x), 0);
-        ctx.lineTo((this.pos.x + CANVAS_WIDTH/2 - this.pos.x), CANVAS_HEIGHT);
+        ctx.moveTo(this.midX, 0);
+        ctx.lineTo(this.midX, CANVAS_HEIGHT);
         ctx.stroke();
         ctx.restore();
     }
