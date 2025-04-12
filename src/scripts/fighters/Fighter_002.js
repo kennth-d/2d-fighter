7adab44cf470e15de4f6f5a2779b5b6458d6f1db
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-12 16:56:31 -0400

    enhance Fighter_002: add AI parameter to constructor for optional AI integration

--- src/scripts/fighters/Fighter_002.js
+++ src/scripts/fighters/Fighter_002.js
@@ -2,10 +2,11 @@ import { FighterBaseClass } from "./FighterBaseClass.js";
 import { FighterStateManager } from "../components/FighterStateManager.js";
 import { FighterSpriteManager } from "../components/SpriteManager.js";
 import { F002_SpriteData } from "../../assets/data/F002_SpriteData.js";
+import { AiStateManager } from "../components/AIStateManager.js";
 
 //Fighter_001 class
 export class Fighter_002 extends FighterBaseClass {
-    constructor(x, y, playerId, inputComponent) {
+    constructor(x, y, playerId, inputComponent, ai) {
         super(x, y, playerId);
         this.name = "F002";
         this.opponent = undefined;
@@ -13,6 +14,7 @@ export class Fighter_002 extends FighterBaseClass {
         this.spriteManager = new FighterSpriteManager(F002_SpriteData);
         this.input = inputComponent;
         this.input.id = playerId;
+        if (ai) this.ai = new AiStateManager(this);
     }//end ctor
 }//end Fighter_001
 
