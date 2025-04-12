220be97c1bb2003f29b0b994f2c44a2063e36b6d
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-12 16:57:58 -0400

    enhance APPROACH state: implement enter, update, and exit methods for AI behavior

--- src/scripts/states/ai/Approach.js
+++ src/scripts/states/ai/Approach.js
@@ -1,3 +1,6 @@
+import { getDistance } from "../../utils/AiUtils.js";
+import { isInStartup } from "../../utils/isInStartup.js";
+import { SP_2 } from "../fighter/AttackStates.js";
 import { AiState } from "./AiState.js";
 
 /**
@@ -9,7 +12,23 @@ export class APPROACH extends AiState {
     constructor(stateName="APPROACH") {
         super(stateName);
     }//end ctor
-    enter() {}
-    update() {}
-    exit() {}
+    enter(manager) {
+        manager.fighter.input.setInput("forward", true);
+    }
+    update(manager) {
+        const distance = getDistance(manager.fighter.pos.x, manager.fighter.opponent.pos.x);
+        if (distance < 26) manager.transition("OBSERVE");
+        const oppState = manager.fighter.opponent.stateManager.getState();
+        if (oppState instanceof SP_2) manager.transition("CROUCHAI");
+        if (isInStartup(manager.fighter.opponent))  {
+            if (distance < 40) {
+                manager.transition("DEFEND");
+            } else {
+                manager.transition("ENGAGE");
+            }//end if-else
+        }//end if
+    }
+    exit(manager) {
+        manager.fighter.input.setInput("forward", false);
+    }
 }//end APPROACH
