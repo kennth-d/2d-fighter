521cdb4f539c12246031dd351b9b6c72021d72be
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-12 16:58:52 -0400

    enhance OBSERVE state: implement enter, update, and exit methods for AI behavior

--- src/scripts/states/ai/Observe.js
+++ src/scripts/states/ai/Observe.js
@@ -1,4 +1,6 @@
 import { AiState } from "./AiState.js";
+import { getDistance, getHealthFactor, getNextAction} from "../../utils/AiUtils.js";
+
 /**
  * OBSERVE state
  * in this state the ai will attempt to get information
@@ -8,7 +10,35 @@ export class OBSERVE extends AiState {
     constructor(stateName="OBSERVE") {
         super(stateName);
     }//end ctor
-    enter() {}
-    update() {}
-    exit() {}
+    enter(manager) {
+        manager.fighter.input.clear();
+    }
+    update(manager) {
+        const hf = getHealthFactor(manager.fighter, -.1, .5) * 100;
+        manager.nextAction = getNextAction(manager.fighter);
+        const distance = getDistance(manager.fighter.pos.x, manager.fighter.opponent.pos.x);
+        if (distance > 50 + hf && !manager.nextAction) manager.transition("APPROACH");
+        
+        if (manager.nextAction === "defend") manager.transition("DEFEND");
+
+        if (manager.nextAction === "attack") manager.transition("ENGAGE");
+
+        if (manager.nextAction === "jump") {
+            const jump = (Math.random() > 0.999);
+            if (jump) manager.transition("JUMP");
+            return;
+        }
+        if (manager.nextAction === "anti-air") {
+            manager.transition("ANTI_AIR");
+            return;
+        } 
+        
+        if (manager.nextAction === "crouch") {
+            manager.transition("CROUCHAI");
+            return;
+        } 
+    }
+    exit(manager) {
+        super.exit();
+    }
 }//end OBVSERVE
