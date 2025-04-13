a0c19fb26a9b722d4aaec34c465d776712c5bc78
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-13 19:14:13 -0400

    fix: format import statement for ENGAGE and ANTI_AIR

--- src/scripts/states/AIStates.js
+++ src/scripts/states/AIStates.js
@@ -1,6 +1,6 @@
 import { OBSERVE } from "./ai/Observe.js";
 import { DEFEND } from "./ai/Defend.js";
-import { ENGAGE, ANTI_AIR } from "./ai/Engage.js";
+import { ENGAGE, ANTI_AIR} from "./ai/Engage.js";
 import { APPROACH } from "./ai/Approach.js";
 import { RETREAT } from "./ai/Retreat.js";
 import { JUMP } from "./ai/Jump.js";
