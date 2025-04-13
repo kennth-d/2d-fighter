1495e24318505588cf1e69c58344de5bb04a6caf
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-13 19:14:20 -0400

    fix: add ATTACK export to fighterStates module

--- src/scripts/states/fighterStates.js
+++ src/scripts/states/fighterStates.js
@@ -1,5 +1,6 @@
 /**module imports all state modules so the can be used under one module..
  * @module states
+ * @exports ATTACK,
  * @exports Idle,
  * @exports WALK_FWD,
  * @exports WALK_BWD,
@@ -22,7 +23,7 @@
  */ 
 
 import { IDLE, WALK_FWD, WALK_BWD, JUMP, JUMP_BWD, JUMP_FWD, CROUCH, FALLING } from "./fighter/MoveStates.js";
-import { LIGHT_ATTACK, HEAVY_ATTACK, SP_1, SP_2, JUMP_ATTACK } from "./fighter/AttackStates.js";
+import { ATTACK, LIGHT_ATTACK, HEAVY_ATTACK, SP_1, SP_2, JUMP_ATTACK } from "./fighter/AttackStates.js";
 import { HURT, KO, KNOCKBACK } from "./fighter/HurtStates.js";
 import { BLOCK } from "./fighter/DefendStates.js";
 
@@ -41,6 +42,7 @@ export {
     JUMP,
     JUMP_BWD,
     JUMP_FWD,
+    ATTACK,
     LIGHT_ATTACK,
     HEAVY_ATTACK,
     SP_1,
