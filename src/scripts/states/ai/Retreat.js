e66bcbeaf34c9840ca559413e8be94b54982063b
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-13 19:15:03 -0400

    fix: implement RETREAT state logic for improved action handling and threat response

--- src/scripts/states/ai/Retreat.js
+++ src/scripts/states/ai/Retreat.js
@@ -1,4 +1,6 @@
 import { AiState } from "./AiState.js";
+import { getWeightedRandom } from "../../utils/AiUtils.js";
+import { TIME } from "../../utils/const.js";
 
 /**
  * RETREAT state
@@ -8,8 +10,25 @@ import { AiState } from "./AiState.js";
 export class RETREAT extends AiState {
     constructor(stateName="RETREAT") {
         super(stateName);
+        this.timer = .5;
     }//end ctor
-    enter() {}
-    update() {}
-    exit() {}
+    enter(manager) {
+        manager.fighter.input.setInput("backward", true);
+    }//end enter
+    update(manager, context) {
+        const opponent = context.opponent;
+        this.timer -= TIME.delta;
+        const threat = opponent.isAttacking;
+        const rangedThreat = opponent.state.getName() === "SP_2";
+        if (threat) manager.transition("DEFEND");
+        if (rangedThreat) manager.transition("CROUCHAI");
+
+        if (context.distance > 50) {
+            const action = getWeightedRandom([{action:"APPROACH", weight:70}, {action:"ENGAGE", weight:30}]);
+            manager.transition(action);
+        } 
+    }//end update
+    exit(manager) {
+        manager.fighter.input.setInput("backward", false);
+    }//end exit
 }//end RETREAT
