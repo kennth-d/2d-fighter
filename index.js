e6fbaab5696e10978085423dba02ba392ce3a490
Kenneth C. White <kenneth.charlesusmc@gmail.com>
2025-04-14 13:38:26 -0400

    updated index.html/css/js

--- index.js
+++ index.js
@@ -1,40 +1,6 @@
-const GameViewport = {
-    WIDTH: 384,
-    HEIGHT: 216,
-}
+import { GameManager } from "../src/scripts/components/GameManager.js";
 
 window.addEventListener('load', function () {
-    //get our canvas and context
-    const canvas = document.querySelector('canvas');
-    const ctx = canvas.getContext('2d');
-
-    //set our canvas size
-    canvas.width = GameViewport.WIDTH;
-    canvas.height = GameViewport.HEIGHT;
-
-    const entities = [
-        new Stage(),
-        new Player(80, 145, 150),
-        new Player2(160, 145, -150),
-        new FPSCounter(),
-    ];
-
-    //for managing game clock
-    const GameTime = {
-        previous: 0,
-        delta: 0,
-    };
-
-    function frame (time) {
-        window.requestAnimationFrame(frame);
-
-        //update time between frames
-        GameTime.delta = (time - GameTime.previous) / 1000;
-        GameTime.previous = time;
-        
-        
-    }//end frame
-
-    window.requestAnimationFrame(frame);
-
+    const game = new GameManager(false);
+    game.start();
 });//end window.onload
